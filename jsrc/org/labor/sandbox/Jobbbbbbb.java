f40fdd2b336722c3bf60b9b7197c09dc13cca7bb
Almond Sandbox <almond.sandbox@gmail.com>
2013-07-26 00:26:44 +0300

    Create DAO's and sql dump.

---
+++ jsrc/org/labor/sandbox/Jobbbbbbb.java
@@ -0,0 +1,28 @@
+package org.labor.sandbox;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class Jobbbbbbb {
+	
+	long job_id;
+	
+	Timestamp begin_time;
+	
+	Timestamp end_time;
+	
+	String description;
+	
+	String perks;
+	
+	String location;
+	
+	float fee;
+	
+	int employer_id;
+	
+	
+}
